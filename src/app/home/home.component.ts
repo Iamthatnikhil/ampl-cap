7fc5ac1c86bb63a5db5d97bf001f4ee64efd60ab
Nikhil Assudani <nikhilasssudani@gmail.com>
2026-08-10 12:02:26 +0530

    harshal updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import jquery from 'jquery';
-
 
 @Component({
   selector: 'app-home',
@@ -11,53 +10,102 @@ import jquery from 'jquery';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  
   response: any;
   renderer: any;
-  hasOpened: any;
-  constructor(private http: HttpClient, private router: Router) { }
-  @ViewChild('ourVision') ourVision!: ElementRef;
-  @ViewChild('ourMission') ourMission!: ElementRef;
-  @ViewChild('ourValues') ourValues!: ElementRef;
+  hasOpened = false;
+  activeSlideIndex = 0;
+  sliderIntervalId: number | null = null;
 
-  vision() {
-    this.ourVision.nativeElement.scrollIntoView({ behavior: 'smooth' });
-  }
-  mission() {
-    this.ourMission.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  sliderItems = [
+    {
+      image: 'assets/img/revolution-slider/nbfc.jpg',
+      alt: 'Non-Bank Financing Services',
+      title: 'Non-Bank <br /><span>Financing</span> Services',
+      text: 'In our vision to become the preferred partner in financial inclusion we are not only providing finances.',
+      link: '/nbfc',
+      buttonText: 'Read More',
+    },
+    {
+      image: 'assets/img/revolution-slider/asset-management.jpg',
+      alt: 'Asset Management',
+      title: 'Asset <br /><span>Management</span>',
+      text: 'We offers a wide range of carefully curated investment portfolios through our PMS and AIF investment structures',
+      link: '/asset-management',
+      buttonText: 'Read More',
+    },
+    {
+      image: 'assets/img/revolution-slider/investment-banking-1.jpg',
+      alt: 'Investment Banking',
+      title: 'Investment <br /><span>Banking</span>',
+      text: 'Our investment banking division is a holistic partner to businesses of all scales and sectors.',
+      link: '/investmentbanking',
+      buttonText: 'Read More',
+    },
+  ];
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  private navigateAndScroll(fragment: string): void {
+    this.router.navigate(['/about-us'], { fragment }).then(() => {
+      setTimeout(() => {
+        const section = document.querySelector(`#${fragment}`) as HTMLElement | null;
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }, 0);
+    });
   }
 
-  values() {
-    this.ourValues.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  vision(): void {
+    this.navigateAndScroll('ourVision');
+  }
+
+  mission(): void {
+    this.navigateAndScroll('ourMission');
+  }
+
+  values(): void {
+    this.navigateAndScroll('ourValues');
   }
 
   article: any;
-  imageUrls: string = `${environment.LocalFilePath}`
-  showModal: boolean = false;
-  
-  
-
+  imageUrls: string = `${environment.LocalFilePath}`;
+  showModal = false;
 
   ngOnInit(): void {
-    jquery(".webinarPopup").addClass("show")
-    jquery("body").addClass("overlay")
+    jquery('.webinarPopup').addClass('show');
+    jquery('body').addClass('overlay');
     this.autoOpenModal();
-
-    this.articleList(); //calling articleList method
+    this.articleList();
+    this.startSlider();
   }
-  
 
-
-  toggleModal() {
-    jquery(".webinarPopup").removeClass("show")
+  ngOnDestroy(): void {
+    if (this.sliderIntervalId !== null) {
+      window.clearInterval(this.sliderIntervalId);
+      this.sliderIntervalId = null;
+    }
   }
-  articleList() {
+
+  startSlider(): void {
+    this.sliderIntervalId = window.setInterval(() => {
+      this.activeSlideIndex = (this.activeSlideIndex + 1) % this.sliderItems.length;
+    }, 5000);
+  }
+
+  toggleModal(): void {
+    jquery('.webinarPopup').removeClass('show');
+  }
+
+  articleList(): void {
     this.http.get(`${environment.ArticleAPI}`).subscribe((response) => {
       console.log(response);
       this.article = response;
     });
   }
-  autoOpenModal() {
-    // Automatically open the modal once after 3 seconds
+
+  autoOpenModal(): void {
     setTimeout(() => {
       if (!this.hasOpened) {
         this.openModal();
@@ -66,12 +114,12 @@ export class HomeComponent {
     }, 3000);
   }
 
-  openModal() {
+  openModal(): void {
     this.showModal = true;
   }
 
-  closeModal() {
+  closeModal(): void {
     this.showModal = false;
   }
-
 }
+
